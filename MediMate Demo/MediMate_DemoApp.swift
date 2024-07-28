0c3972dc1ab2178542db4f584f22ed827ff701ea
Jabin0214 <jabinchen0214@gmail.com>
2024-07-28 23:49:21 +1200

    Initial Commit

---
+++ MediMate Demo/MediMate_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediMate_DemoApp.swift
+//  MediMate Demo
+//
+//  Created by Jabin on 2024/7/28.
+//
+
+import SwiftUI
+
+@main
+struct MediMate_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
