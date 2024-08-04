4c1a4bb4e84ca5737123d21964d010c34a5e69c0
Jabin0214 <jabinchen0214@gmail.com>
2024-08-05 11:20:37 +1200

    file create

---
+++ Medimate/Medimate/MedimateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedimateApp.swift
+//  Medimate
+//
+//  Created by Jabin on 2024/8/5.
+//
+
+import SwiftUI
+
+@main
+struct MedimateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
