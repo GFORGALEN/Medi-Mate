66e2c2a5f602acac0da3b490c762487b5f47bc62
TZzzzzQAQ <tzzzqaq@gmail.com>
2024-08-05 21:15:15 +1200

    v1.5 fix bug

--- backend/src/main/java/com/friedchicken/controller/app/UserController.java
+++ backend/src/main/java/com/friedchicken/controller/app/UserController.java
@@ -8,24 +8,32 @@ import com.friedchicken.properties.JwtProperties;
 import com.friedchicken.result.Result;
 import com.friedchicken.service.UserService;
 import com.friedchicken.utils.JwtUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api/user")
+@Tag(name = "JWT Controller", description = "API for JWT operations")
 @Slf4j
 public class UserController {
     @Autowired
     private UserService userService;
     @Autowired
     private JwtProperties jwtProperties;
+    @Autowired
+    private JwtUtil jwtUtil;
 
     @PostMapping("/login")
+    @Operation(summary = "User Login", description = "Generates a JWT for user login")
     public Result<UserLoginVO> login(@RequestBody UserLoginDTO userLoginDTO){
         log.info("A user want to login in:{}",userLoginDTO.toString());
 
@@ -34,7 +42,7 @@ public class UserController {
         Map<String,Object> claims=new HashMap<>();
 
         claims.put(JwtClaimsConstant.USER_ID,user.getUserId());
-        String token= JwtUtil.createJWT(jwtProperties.getUserSecretKey(),jwtProperties.getUserTtl(),claims);
+        String token= jwtUtil.generateUserToken(user.getUsername(),claims);
 
         UserLoginVO userLoginVO = UserLoginVO.builder()
                 .userId(user.getUserId())
--- backend/src/main/java/com/friedchicken/utils/JwtUtil.java
+++ backend/src/main/java/com/friedchicken/utils/JwtUtil.java
@@ -1,57 +1,59 @@
 package com.friedchicken.utils;
 
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import java.nio.charset.StandardCharsets;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class JwtUtil {
-    /**
-     * 生成jwt
-     * 使用Hs256算法, 私匙使用固定秘钥
-     *
-     * @param secretKey jwt秘钥
-     * @param ttlMillis jwt过期时间(毫秒)
-     * @param claims    设置的信息
-     * @return
-     */
-    public static String createJWT(String secretKey, long ttlMillis, Map<String, Object> claims) {
-        // 指定签名的时候使用的签名算法，也就是header那部分
-        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
 
-        // 生成JWT的时间
-        long expMillis = System.currentTimeMillis() + ttlMillis;
-        Date exp = new Date(expMillis);
+    @Value("${fc.jwt.admin-secret-key}")
+    private String adminSecretKey;
 
-        // 设置jwt的body
-        JwtBuilder builder = Jwts.builder()
-                // 如果有私有声明，一定要先设置这个自己创建的私有的声明，这个是给builder的claim赋值，一旦写在标准的声明赋值之后，就是覆盖了那些标准的声明的
+    @Value("${fc.jwt.admin-ttl}")
+    private long adminTtl;
+
+    @Value("${fc.jwt.user-secret-key}")
+    private String userSecretKey;
+
+    @Value("${fc.jwt.user-ttl}")
+    private long userTtl;
+
+    public String generateAdminToken(String username,Map<String, Object> claims) {
+        return generateToken(username, adminSecretKey, adminTtl,claims);
+    }
+
+    public String generateUserToken(String username,Map<String, Object> claims) {
+        return generateToken(username, userSecretKey, userTtl,claims);
+    }
+
+    private String generateToken(String username, String secretKey, long ttl,Map<String, Object> claims) {
+        return Jwts.builder()
                 .setClaims(claims)
-                // 设置签名使用的签名算法和签名使用的秘钥
-                .signWith(signatureAlgorithm, secretKey.getBytes(StandardCharsets.UTF_8))
-                // 设置过期时间
-                .setExpiration(exp);
-        return builder.compact();
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + ttl))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
     }
 
-    /**
-     * Token解密
-     *
-     * @param secretKey jwt秘钥 此秘钥一定要保留好在服务端, 不能暴露出去, 否则sign就可以被伪造, 如果对接多个客户端建议改造成多个
-     * @param token     加密后的token
-     * @return
-     */
-    public static Claims parseJWT(String secretKey, String token) {
-        // 得到DefaultJwtParser
-        Claims claims = Jwts.parser()
-                // 设置签名的秘钥
-                .setSigningKey(secretKey.getBytes(StandardCharsets.UTF_8))
-                // 设置需要解析的jwt
-                .parseClaimsJws(token).getBody();
-        return claims;
+    public boolean validateToken(String token, String secretKey) {
+        try {
+            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
+    public String getUsernameFromToken(String token, String secretKey) {
+        Claims claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+        return claims.getSubject();
+    }
 }
