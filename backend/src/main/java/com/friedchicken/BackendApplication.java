2a907a57bb9000f8efe77cd92ad1eb7b90cf82f4
TZzzzzQAQ <tzzzqaq@gmail.com>
2024-08-05 12:12:45 +1200

    v1.1 Create Project

--- backend/src/main/java/com/friedchicken/BackendApplication.java
+++ backend/src/main/java/com/friedchicken/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.friedchicken.backend;
+package com.friedchicken;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(BackendApplication.class, args);
     }
 
