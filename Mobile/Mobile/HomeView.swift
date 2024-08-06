73bbde861bd061c11b6ab5b44de18b1e59284e18
Jabin0214 <jabinchen0214@gmail.com>
2024-08-06 20:34:22 +1200

    v1.3

---
+++ Mobile/Mobile/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Mobile
+//
+//  Created by Jabin on 2024/8/6.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ Mobile/Mobile/RegisterView.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterView.swift
+//  Mobile
+//
+//  Created by Jabin on 2024/8/6.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegisterView()
+}
