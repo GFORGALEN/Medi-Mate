72597e4b1754e43558eca7efcd6a4637cddcf45f
ltai457 <114079256+ltai457@users.noreply.github.com>
2024-08-06 18:50:14 +1200

    Testing

--- Mobile/Mobile/LoginView.swift
+++ Mobile/Mobile/LoginView.swift
@@ -5,4 +5,45 @@
 //  Created by Jabin on 2024/8/6.
 //
 
-import Foundation
+//
+//  ContentView.swift
+//  Mobile
+//
+//  Created by Jabin on 2024/8/6.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        TabView{
+            
+            Text("Home")
+                .toolbarBackground(.visible, for: .tabBar)
+                .tabItem {
+                    Label("Home",
+                    systemImage: "house")}
+            
+            Text("bag")
+                .toolbarBackground(.visible, for: .tabBar)
+                .tabItem { Label("shop",systemImage: "bag.fill") }
+            
+            Text("Cart")
+                .toolbarBackground(.visible, for: .tabBar)
+                .tabItem {
+                    Label("cart",
+                    systemImage: "cart.fill")}
+           
+            Text("Account")
+                .toolbarBackground(.visible, for: .tabBar)
+                .tabItem {
+                Label("Account",
+                systemImage: "person.crop.circle")}
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
