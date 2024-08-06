6cfac99f24c04a3ebc0653ea01072f4c644fda60
Jabin0214 <jabinchen0214@gmail.com>
2024-08-06 18:21:23 +1200

    test

---
+++ Mobile/Mobile/MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileApp.swift
+//  Mobile
+//
+//  Created by Jabin on 2024/8/6.
+//
+
+import SwiftUI
+
+@main
+struct MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
