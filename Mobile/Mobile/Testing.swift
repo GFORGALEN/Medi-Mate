7427f34d7f230e1ac054372eabe6fbf84c39bf2d
ltai457 <ltai457@aucklanduni.ac.nz>
2024-08-07 17:58:37 +1200

    Test file

---
+++ Mobile/Mobile/Testing.swift
@@ -0,0 +1,51 @@
+//
+//  Testing.swift
+//  Mobile
+//
+//  Created by Lykheang Taing on 07/08/2024.
+//
+
+import Foundation
+import SwiftUI
+
+struct ContentView: View {
+    //@State private var errorMessage: String = ""
+    @State private var isAuthenticated: Bool = false  // Tracks the authentication status
+
+    var body: some View {
+        TabView {
+            // Home View Tab
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+            
+            // Shop View Tab
+            Text("Shop")
+                .tabItem {
+                    Label("Shop", systemImage: "bag.fill")
+                }
+            
+            // Cart View Tab
+            Text("Cart")
+                .tabItem {
+                    Label("Cart", systemImage: "cart.fill")
+                }
+            LoginView()
+            
+                .tabItem {
+                    Label("Account", systemImage: "person.crop.circle")
+                }
+        }
+            
+//
+        .toolbarBackground(.visible, for: .tabBar)
+    }
+}
+
+// Preview
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
