9a89ae545f6d37bcf98295c25b9503ca150e9e9f
Jabin0214 <jabinchen0214@gmail.com>
2024-08-07 14:48:48 +1200

    v1.5

---
+++ Mobile/Mobile/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  constant.swift
+//  Mobile
+//
+//  Created by Jabin on 2024/8/7.
+//
+
+import Foundation
+
+enum{
+    
+}
---
+++ Mobile/Mobile/ResultView.swift
@@ -0,0 +1,18 @@
+//
+//  ResultView.swift
+//  Mobile
+//
+//  Created by Jabin on 2024/8/7.
+//
+
+import SwiftUI
+
+struct ResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResultView()
+}
