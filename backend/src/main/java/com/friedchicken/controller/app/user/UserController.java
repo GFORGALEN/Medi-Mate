5a80c2d5b6a899192a5c4e8028eed98ab2f7e766
TZzzzzQAQ <tzzzqaq@gmail.com>
2024-08-08 12:58:01 +1200

    v1.0 product page api

--- backend/src/main/java/com/friedchicken/controller/app/user/UserController.java
+++ backend/src/main/java/com/friedchicken/controller/app/user/UserController.java
@@ -1,4 +1,4 @@
-package com.friedchicken.controller.app;
+package com.friedchicken.controller.app.user;
 
 import com.friedchicken.constant.MessageConstant;
 import com.friedchicken.exception.AccountNotFoundException;
--- backend/src/main/java/com/friedchicken/result/PageResult.java
+++ backend/src/main/java/com/friedchicken/result/PageResult.java
@@ -7,16 +7,13 @@ import lombok.NoArgsConstructor;
 import java.io.Serializable;
 import java.util.List;
 
-/**
- * 封装分页查询结果
- */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class PageResult implements Serializable {
+public class PageResult<T> implements Serializable {
 
     private long total; //总记录数
 
-    private List records; //当前页数据集合
+    private List<T> records; //当前页数据集合
 
 }
