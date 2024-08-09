0fb648af831b58cb54c4cb32bca5d6177cddf305
ltai457 <ltai457@aucklanduni.ac.nz>
2024-08-09 13:25:26 +1200

    Camera

--- Mobile/Mobile/HomeView.swift
+++ Mobile/Mobile/HomeView.swift
@@ -1,48 +1,85 @@
-//
-//  HomeView.swift
-//  Mobile
-//
-//  Created by Jabin on 2024/8/6.
-//
-
 import SwiftUI
+import UIKit
 
 struct HomeView: View {
     @State private var text: String = ""
+    @State private var image: UIImage?
+    @State private var isShowingCamera = false
 
     var body: some View {
-        VStack{
-                Text("Medimate")
+        VStack {
+            Text("Medimate")
                 .font(.largeTitle)
                 .bold()
 
-        HStack {
-            TextField("Type something...", text: $text)
-            
-            Button(action: {
-                // Action for the camera button
-            }) {
-                Image(.cameraRetroSolid)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 30, height: 30)
+            HStack {
+                TextField("Type something...", text: $text)
+                
+                Button(action: {
+                    self.isShowingCamera = true
+                }) {
+                    Image(.cameraRetroSolid)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 30, height: 30)
+                }
+                .background(Color.white)
+                .cornerRadius(10)
             }
-            .background(Color.white)
-            .cornerRadius(10)
-            
-        }
-        .padding()
-        .overlay(
-                    RoundedRectangle(cornerRadius: 10)
+            .padding()
+            .overlay(
+                RoundedRectangle(cornerRadius: 10)
                     .stroke(Color.black, lineWidth: 4)
-                        )
-        .padding(.bottom, 200)
-        .padding()
+            )
+            .padding(.bottom, 200)
+            .padding()
 
+            if let image = image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+            }
+        }
+        .sheet(isPresented: $isShowingCamera) {
+            ImagePicker(image: self.$image, sourceType: .camera)
+        }
     }
 }
-}
 
+struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    @Environment(\.presentationMode) var presentationMode
+    var sourceType: UIImagePickerController.SourceType
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.sourceType = sourceType
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        let parent: ImagePicker
+
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let uiImage = info[.originalImage] as? UIImage {
+                parent.image = uiImage
+            }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
 
 #Preview {
     HomeView()
