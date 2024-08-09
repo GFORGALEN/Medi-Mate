73d9fa555c4f5b365f9b430dfd59560101731480
Jabin0214 <jabinchen0214@gmail.com>
2024-08-09 16:31:17 +1200

    v1.6

---
+++ Mobile/Mobile/ChatView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatView.swift
+//  Mobile
+//
+//  Created by Jabin on 2024/8/9.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatView()
+}
---
+++ Mobile/Mobile/ResultShowView.swift
@@ -0,0 +1,18 @@
+//
+//  ResultShowView.swift
+//  Mobile
+//
+//  Created by Jabin on 2024/8/9.
+//
+
+import SwiftUI
+
+struct ResultShowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResultShowView()
+}
