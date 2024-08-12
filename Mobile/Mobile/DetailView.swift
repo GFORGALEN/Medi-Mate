89c06bd9ec7d73ea91a662375e74d232aa7c3da9
Jabin0214 <jabinchen0214@gmail.com>
2024-08-12 14:05:29 +1200

    v1.9

---
+++ Mobile/Mobile/DetailView.swift
@@ -0,0 +1,8 @@
+//
+//  DetailView.swift
+//  Mobile
+//
+//  Created by Jabin on 2024/8/12.
+//
+
+import Foundation
---
+++ Mobile/Mobile/NewScreenView.swift
@@ -0,0 +1,8 @@
+//
+//  NewScreenView.swift
+//  Mobile
+//
+//  Created by Jabin on 2024/8/12.
+//
+
+import Foundation
