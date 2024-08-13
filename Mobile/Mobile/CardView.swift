f19b6886563b26bff98e41a69ad583a5c0f10fad
Jabin0214 <jabinchen0214@gmail.com>
2024-08-13 18:12:00 +1200

    v2.0.1

---
+++ Mobile/Mobile/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  Mobile
+//
+//  Created by Jabin on 2024/8/13.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
