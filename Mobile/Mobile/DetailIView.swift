bb181d5ceace3fea7dda1c07976dae24dceaa62a
Jabin0214 <jabinchen0214@gmail.com>
2024-08-14 17:41:14 +1200

    v2.4

---
+++ Mobile/Mobile/DetailIView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailIView.swift
+//  Mobile
+//
+//  Created by Jabin on 2024/8/14.
+//
+
+import SwiftUI
+
+struct DetailIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailIView()
+}
