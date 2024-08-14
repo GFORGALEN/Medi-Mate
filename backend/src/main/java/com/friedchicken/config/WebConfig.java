b2b53e3f0837b90159852f36ca1d56d56bf438f6
TZzzzzQAQ <tzzzqaq@gmail.com>
2024-08-14 12:45:56 +1200

    v1.5 fix bugs

--- backend/src/main/java/com/friedchicken/config/WebConfig.java
+++ backend/src/main/java/com/friedchicken/config/WebConfig.java
@@ -4,6 +4,7 @@ package com.friedchicken.config;
 import com.friedchicken.interceptor.LoginInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -28,4 +29,12 @@ public class WebConfig implements WebMvcConfigurer {
                 "/swagger-ui/**");
     }
 
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")  // 允许所有来源
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                .allowedHeaders("*")
+//                .allowCredentials(true);
+//    }
 }
