1f6f8fbd84b3bb6244a49b37178b769cdeb96bfc
Jabin0214 <jabinchen0214@gmail.com>
2024-08-27 17:06:06 +1200

    v0.2.3

--- web/src/layouts/HeaderLayout.jsx
+++ web/src/layouts/HeaderLayout.jsx
@@ -1,14 +1,35 @@
 import React from 'react';
+import { Button } from 'antd';
+import { LogoutOutlined } from '@ant-design/icons';
+import { useNavigate } from 'react-router-dom';
+
 
 const HeaderLayout = () => {
+
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        console.log('User logged out');
+        navigate('/login');
+    };
+
     return (
-        <div className="flex-grow text-center py-4">
-            <h1 className="text-4xl font-semibold text-gray-800 tracking-wide font-poppins">
-                <span className="text-blue-600 font-bold ml-2">
-                    Medimate
-                </span>
-                {' '}Management System 
-            </h1>
+        <div className="flex items-center justify-between w-full px-4">
+                <h1 className="text-2xl md:text-3xl lg:text-4xl font-semibold text-gray-800 tracking-wide font-poppins">
+                    <span className="text-blue-600 font-bold">
+                        Medimate
+                    </span>
+                    {' '}Management System
+                </h1>
+            <Button 
+                type="primary" 
+                icon={<LogoutOutlined />} 
+                onClick={handleLogout} 
+                danger
+                className="ml-4"
+            >
+                Logout
+            </Button>
         </div>
     );
 };
