0710d81637023513aca6142513f6a3b7cff1476c
ltai457 <ltai457@aucklanduni.ac.nz>
2024-08-28 10:41:40 +1200

    UserInfo

---
+++ Mobile/Mobile/Authentication/ChangePassWordView.swift
@@ -0,0 +1,8 @@
+//
+//  ChangePassWordView.swift
+//  Mobile
+//
+//  Created by Lykheang Taing on 28/08/2024.
+//
+
+import Foundation
---
+++ Mobile/Mobile/Authentication/EditProfileView.swift
@@ -0,0 +1,8 @@
+//
+//  EditProfileView.swift
+//  Mobile
+//
+//  Created by Lykheang Taing on 28/08/2024.
+//
+
+import Foundation
