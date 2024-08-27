80064f8a74288fa22813a5adb8ad202c33d934f4
GFORGALEN <100574277+GFORGALEN@users.noreply.github.com>
2024-08-27 17:19:30 +1200

    finish web1.8 finished some bug

--- web/src/pages/PharmacyCard.jsx
+++ web/src/pages/PharmacyCard.jsx
@@ -1,104 +1,149 @@
-import { useState } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import  { useState, useCallback, useEffect } from 'react';
 import { GoogleMap, Marker } from '@react-google-maps/api';
 import { useNavigate } from 'react-router-dom';
-import { Spin } from 'antd';
-import { ModelComponents } from './PharmacyModels';
+import { Spin, Button, message } from 'antd';
+import { LeftOutlined, RightOutlined } from '@ant-design/icons';
 import { getImagePath, getGoogleMapsUrl } from '../utils/utils';
-import PropTypes from "prop-types";
+import { getPharmaciesAPI } from '@/api/user/pharmacy';
+import useGoogleMapsApi from '@/hook/useGoogleMapsApi.jsx'; // 假设这是您的自定义hook
 
-const PharmacyCard = ({ pharmacy, index, isMapLoaded }) => {
+const PharmacySingleMapCard = () => {
     const navigate = useNavigate();
-    const [isImageExpanded, setIsImageExpanded] = useState(false);
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [map, setMap] = useState(null);
+    const [pharmacies, setPharmacies] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const isMapLoaded = useGoogleMapsApi();
+
+    useEffect(() => {
+        const fetchPharmacies = async () => {
+            try {
+                const response = await getPharmaciesAPI();
+                if (response.code === 1 && Array.isArray(response.data)) {
+                    setPharmacies(response.data);
+                } else {
+                    throw new Error('Invalid response format');
+                }
+            } catch (error) {
+                console.error('Error fetching pharmacies:', error);
+                message.error('Failed to load pharmacy data');
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchPharmacies();
+    }, []);
 
     const mapContainerStyle = {
         width: '100%',
-        height: '300px'
+        height: 'calc(100vh - 200px)' // Adjust this value based on your card height
     };
 
-    const center = {
-        lat: parseFloat(pharmacy.latitude),
-        lng: parseFloat(pharmacy.longitude)
-    };
+    const defaultCenter = { lat: -36.8485, lng: 174.7633 }; // Auckland coordinates
 
-    const ModelComponent = ModelComponents[index % ModelComponents.length];
+    const currentPharmacy = pharmacies[currentIndex] || null;
+    const center = currentPharmacy
+        ? { lat: parseFloat(currentPharmacy.latitude), lng: parseFloat(currentPharmacy.longitude) }
+        : defaultCenter;
 
     const handleViewInventory = () => {
-        navigate(`/inventory/${pharmacy.pharmacyId}`);
+        if (currentPharmacy) {
+            navigate(`/inventory/${currentPharmacy.pharmacyId}`);
+        }
     };
 
-    return (
-        <div className="w-full bg-white rounded-xl shadow-md overflow-hidden mb-8">
-            <div className="flex flex-col">
-                <div
-                    className="overflow-hidden cursor-pointer transition-all duration-300 ease-in-out"
-                    style={{ height: isImageExpanded ? '85vh' : '40vh' }}
-                    onClick={() => setIsImageExpanded(!isImageExpanded)}
-                >
-                    <img
-                        src={getImagePath(pharmacy.pharmacyName)}
-                        alt={pharmacy.pharmacyName}
-                        className="w-full h-full object-cover"
-                    />
-                </div>
-                <div className="p-8">
-                    <div className="uppercase tracking-wide text-2xl text-indigo-500 font-semibold">
-                        {pharmacy.pharmacyName}
-                    </div>
-                    <a
-                        href={getGoogleMapsUrl(pharmacy.pharmacyAddress)}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="mt-2 text-gray-500 text-lg hover:text-indigo-600 transition duration-150 ease-in-out"
-                    >
-                        {pharmacy.pharmacyAddress}
-                    </a>
-                    <div className="h-96 relative bg-gray-100 mt-4">
-                        <Canvas camera={{position: [0, 0, 5], fov: 60}}>
-                            <ambientLight intensity={0.7}/>
-                            <pointLight position={[10, 10, 10]} intensity={1}/>
-                            <ModelComponent/>
-                            <OrbitControls enablePan={false} enableZoom={false}/>
-                        </Canvas>
-                    </div>
-                    <div className="mt-6 h-80">
-                        {isMapLoaded ? (
-                            <GoogleMap
-                                mapContainerStyle={mapContainerStyle}
-                                center={center}
-                                zoom={15}
-                            >
-                                <Marker position={center}/>
-                            </GoogleMap>
-                        ) : (
-                            <div className="w-full h-full flex items-center justify-center bg-gray-200">
-                                <Spin/>
-                            </div>
-                        )}
-                    </div>
-                    <button
-                        onClick={handleViewInventory}
-                        className="mt-6 w-full px-6 py-3 border border-transparent text-lg font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition duration-150 ease-in-out"
-                    >
-                        View Inventory
-                    </button>
-                </div>
+    const handleNext = useCallback(() => {
+        if (pharmacies.length > 0) {
+            setCurrentIndex((prevIndex) => (prevIndex + 1) % pharmacies.length);
+        }
+    }, [pharmacies.length]);
+
+    const handlePrevious = useCallback(() => {
+        if (pharmacies.length > 0) {
+            setCurrentIndex((prevIndex) => (prevIndex - 1 + pharmacies.length) % pharmacies.length);
+        }
+    }, [pharmacies.length]);
+
+    useEffect(() => {
+        if (map && currentPharmacy) {
+            map.panTo(center);
+        }
+    }, [currentIndex, map, center, currentPharmacy]);
+
+    const onMapLoad = useCallback((map) => {
+        setMap(map);
+    }, []);
+
+    if (loading) {
+        return (
+            <div className="w-full h-screen flex items-center justify-center bg-gray-200">
+                <Spin size="large" />
             </div>
+        );
+    }
+
+    if (pharmacies.length === 0) {
+        return (
+            <div className="w-full h-screen flex items-center justify-center bg-gray-200">
+                <p>No pharmacies data available.</p>
+            </div>
+        );
+    }
+
+    return (
+        <div className="w-full h-screen flex flex-col">
+            {isMapLoaded ? (
+                <GoogleMap
+                    mapContainerStyle={mapContainerStyle}
+                    center={center}
+                    zoom={15}
+                    onLoad={onMapLoad}
+                >
+                    {currentPharmacy && <Marker position={center} />}
+                </GoogleMap>
+            ) : (
+                <div className="w-full h-full flex items-center justify-center bg-gray-200">
+                    <Spin />
+                </div>
+            )}
+
+            {currentPharmacy && (
+                <div className="bg-white shadow-md overflow-hidden">
+                    <div className="flex p-4 items-center">
+                        <Button icon={<LeftOutlined />} onClick={handlePrevious} className="mr-2" />
+                        <div className="flex-grow flex">
+                            <img
+                                src={getImagePath(currentPharmacy.pharmacyName)}
+                                alt={currentPharmacy.pharmacyName}
+                                className="w-1/3 h-40 object-cover rounded-lg mr-4"
+                            />
+                            <div className="w-2/3">
+                                <div className="text-xl text-indigo-500 font-semibold">
+                                    {currentPharmacy.pharmacyName}
+                                </div>
+                                <a
+                                    href={getGoogleMapsUrl(currentPharmacy.pharmacyAddress)}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="mt-2 block text-gray-500 text-base hover:text-indigo-600 transition duration-150 ease-in-out"
+                                >
+                                    {currentPharmacy.pharmacyAddress}
+                                </a>
+                                <button
+                                    onClick={handleViewInventory}
+                                    className="mt-4 w-full px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition duration-150 ease-in-out"
+                                >
+                                    View Inventory
+                                </button>
+                            </div>
+                        </div>
+                        <Button icon={<RightOutlined />} onClick={handleNext} className="ml-2" />
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
 
-PharmacyCard.propTypes = {
-    pharmacy: PropTypes.shape({
-        pharmacyId: PropTypes.string.isRequired,
-        pharmacyName: PropTypes.string.isRequired,
-        pharmacyAddress: PropTypes.string.isRequired,
-        latitude: PropTypes.string.isRequired,
-        longitude: PropTypes.string.isRequired,
-    }).isRequired,
-    index: PropTypes.number.isRequired,
-    isMapLoaded: PropTypes.bool.isRequired,
-};
-
-export default PharmacyCard;
+export default PharmacySingleMapCard;
