7d3dd32cd445b88c60c6bc0c86dcaa79c155d1df
TZzzzzQAQ <tzzzqaq@gmail.com>
2024-09-05 15:16:14 +1200

    v3.0 order api

---
+++ backend/src/main/java/com/friedchicken/service/OrderService.java
@@ -0,0 +1,8 @@
+package com.friedchicken.service;
+
+import com.friedchicken.pojo.dto.Order.OrderDTO;
+
+public interface OrderService {
+
+    void orderProduct(OrderDTO orderDTO);
+}
---
+++ backend/src/main/java/com/friedchicken/service/impl/OrderServiceImpl.java
@@ -0,0 +1,41 @@
+package com.friedchicken.service.impl;
+
+import com.friedchicken.mapper.OrderMapper;
+import com.friedchicken.pojo.dto.Order.OrderDTO;
+import com.friedchicken.pojo.dto.Order.OrderItemDTO;
+import com.friedchicken.pojo.entity.Order.Order;
+import com.friedchicken.pojo.entity.Order.OrderItem;
+import com.friedchicken.service.OrderService;
+import com.friedchicken.utils.UniqueIdUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+public class OrderServiceImpl implements OrderService {
+    @Autowired
+    private OrderMapper orderMapper;
+    @Autowired
+    private UniqueIdUtil uniqueIdUtil;
+
+    @Override
+    @Transactional
+    public void orderProduct(OrderDTO orderDTO) {
+        Order order = new Order();
+        BeanUtils.copyProperties(orderDTO, order);
+        String orderId = uniqueIdUtil.generateUniqueId();
+        order.setOrderId(orderId);
+        order.setStatus(1);
+        orderMapper.insertOrder(order);
+        for (OrderItemDTO item : orderDTO.getOrderItem()) {
+            OrderItem orderItem = new OrderItem();
+            BeanUtils.copyProperties(item, orderItem);
+            orderItem.setOrderId(orderId);
+            orderItem.setItemId(uniqueIdUtil.generateUniqueId());
+            orderMapper.insertOrderItem(orderItem);
+        }
+    }
+}
