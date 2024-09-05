9869f9b2268b18caeef5370cff316c6a49cb91a9
Jabin0214 <jabinchen0214@gmail.com>
2024-09-05 14:31:33 +1200

    v3.0.0

---
+++ Mobile/Mobile/Result/LocationView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationView.swift
+//  Mobile
+//
+//  Created by Jabin on 2024/9/5.
+//
+
+import SwiftUI
+
+struct LocationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationView()
+}
