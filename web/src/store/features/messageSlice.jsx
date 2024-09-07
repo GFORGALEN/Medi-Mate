128da56cd21e9f193fb808b5db0386952c025805
Jabin0214 <jabinchen0214@gmail.com>
2024-09-07 19:40:31 +1200

    web: order duplicate check

--- web/src/store/features/messageSlice.jsx
+++ web/src/store/features/messageSlice.jsx
@@ -3,7 +3,7 @@ import {createSlice} from '@reduxjs/toolkit';
 export const messageSlice = createSlice({
     name: 'message',
     initialState: {
-        orderId: ""
+        orderId: "3392b041-d673-48d9-a2b0-1e3b2e376704"
     },
     reducers: {
         setOrderId: (state, action) => {
