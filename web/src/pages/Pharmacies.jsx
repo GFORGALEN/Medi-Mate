0c81b7139df09772f15e12a1376c80e9fd1ce177
GFORGALEN <100574277+GFORGALEN@users.noreply.github.com>
2024-09-08 21:09:28 +1200

    Never been able to fix Google map showmarker, I replaced the map api, still need to confirm if it's modified or not

--- web/src/pages/Pharmacies.jsx
+++ web/src/pages/Pharmacies.jsx
@@ -1,9 +1,9 @@
-import { useState, useCallback } from 'react';
+import React, { useState, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button } from 'antd';
 import { LeftOutlined, RightOutlined } from '@ant-design/icons';
 import { getImagePath, getGoogleMapsUrl } from '../utils/utils';
-import PharmacyMapWithCards from '@/pages/PharmacyCard.jsx';
+import LeafletPharmacyMap from './LeafletPharmacyMap';
 
 const Pharmacies = () => {
     const navigate = useNavigate();
@@ -34,22 +34,24 @@ const Pharmacies = () => {
 
     return (
         <div className="relative w-full h-screen">
-            <PharmacyMapWithCards
-                onPharmaciesLoaded={handlePharmaciesLoaded}
-                currentIndex={currentIndex}
-            />
+            <div className="absolute inset-0 z-0">
+                <LeafletPharmacyMap
+                    onPharmaciesLoaded={handlePharmaciesLoaded}
+                    currentIndex={currentIndex}
+                />
+            </div>
 
             {pharmacies[currentIndex] && (
-                <div className="absolute top-4 left-1/2 transform -translate-x-1/2 w-8/12 md:w-1/2 lg:w-4/12 lg:h-1/8 bg-white rounded-xl shadow-lg overflow-hidden transition-all duration-300 ease-in-out">
+                <div className="absolute top-4 left-1/2 transform -translate-x-1/2 w-11/12 md:w-3/4 lg:w-1/2 bg-white rounded-xl shadow-lg overflow-hidden transition-all duration-300 ease-in-out z-10">
                     <div className="flex p-4 items-center">
                         <Button icon={<LeftOutlined />} onClick={handlePrevious} className="mr-2" />
                         <div className="flex-grow flex items-center">
                             <img
                                 src={getImagePath(pharmacies[currentIndex].pharmacyName)}
                                 alt={pharmacies[currentIndex].pharmacyName}
-                                className="w-1/3 h-40 object-cover rounded-lg mr-4"
+                                className="w-1/4 h-32 object-cover rounded-lg mr-4"
                             />
-                            <div className="w-2/3">
+                            <div className="w-3/4">
                                 <div className="text-xl text-indigo-500 font-semibold">
                                     {pharmacies[currentIndex].pharmacyName}
                                 </div>
