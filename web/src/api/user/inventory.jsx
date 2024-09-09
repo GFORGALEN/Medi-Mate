52a25b4912a952f0af68a8023ca77f1dbcae2343
GFORGALEN <100574277+GFORGALEN@users.noreply.github.com>
2024-09-09 13:57:17 +1200

    fix bug

--- web/src/api/user/inventory.jsx
+++ web/src/api/user/inventory.jsx
@@ -1,12 +1,13 @@
 import { requestInventory } from "@/axios/requestInventory";
 
-export function getInventoryAPI(pharmacyId, page, pageSize) {
+export function getInventoryAPI(pharmacyId, page, pageSize,productName) {
     return requestInventory({
         method: 'POST',
         data: {
             pharmacyId,
             page,
-            pageSize
+            pageSize,
+            productName
         }
     });
 }
